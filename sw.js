0a1215f099a092222fdff13fe6bb5cd73dd24017
shawonahmadsaddat <77286918+shawonahmadsaddat@users.noreply.github.com>
2026-09-14 12:44:28 +0600

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='cb-financial-calculator-v3';
-const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{if(e.request.method==='GET'){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));}return r}).catch(()=>caches.match(e.request))));
